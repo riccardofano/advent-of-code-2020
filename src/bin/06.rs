f8f91051d80144efed7e88d04452a643d7d434ee
Riccardo Fano <rcr.fano@gmail.com>
2023-08-23 12:17:34 +0200

    Solve second part of day 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -20,7 +20,23 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    None
+    let sum = input
+        .split("\n\n")
+        .map(|group| {
+            let group_lines = group.lines().collect::<Vec<_>>();
+            let mut map: HashMap<u8, usize> = HashMap::new();
+
+            for line in &group_lines {
+                for byte in line.as_bytes() {
+                    *map.entry(*byte).or_default() += 1;
+                }
+            }
+
+            map.values().filter(|v| *v == &group_lines.len()).count()
+        })
+        .sum();
+
+    Some(sum)
 }
 
 fn main() {
@@ -42,6 +58,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 6);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(6));
     }
 }
