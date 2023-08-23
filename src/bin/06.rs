475ea70104fe00b135d7513734666a0168321be0
Riccardo Fano <rcr.fano@gmail.com>
2023-08-23 12:14:43 +0200

    Solve first part of day 6

---
+++ src/bin/06.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let sum = input
+        .split("\n\n")
+        .map(|group| {
+            let mut map: HashMap<u8, usize> = HashMap::new();
+
+            for line in group.lines() {
+                for byte in line.as_bytes() {
+                    *map.entry(*byte).or_default() += 1;
+                }
+            }
+
+            map.values().len()
+        })
+        .sum();
+
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 6);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 6);
+        assert_eq!(part_one(&input), Some(11));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 6);
+        assert_eq!(part_two(&input), None);
+    }
+}
