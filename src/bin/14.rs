ffecd565a3895de90e4bf1fee1297f441835944a
Riccardo Fano <rcr.fano@gmail.com>
2023-08-25 20:42:08 +0200

    Solve second part of day 14

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -13,6 +13,20 @@ fn mask_value(mask: &str, mut value: u64) -> u64 {
     value
 }
 
+fn apply_memory_mask(mask: &str, mut address: u64) -> (Vec<usize>, u64) {
+    let mut floating_bits = Vec::new();
+    for (i, bit) in mask.bytes().rev().enumerate() {
+        match bit {
+            b'0' => continue,
+            b'1' => address |= 1 << i,
+            b'X' => floating_bits.push(i),
+            _ => unreachable!(),
+        }
+    }
+
+    (floating_bits, address)
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
     let mut lines = input.lines();
 
@@ -35,7 +49,39 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut lines = input.lines();
+
+    let mut current_mask = lines.next().unwrap().split_at(7).1;
+    let mut memory: HashMap<u64, u64> = HashMap::new();
+
+    for line in lines {
+        match line.split_once(" = ").unwrap() {
+            ("mask", bits) => current_mask = bits,
+            (memory_address, value) => {
+                let original_address = memory_address[4..memory_address.len() - 1].parse().unwrap();
+                let value = value.parse().unwrap();
+
+                let (floating_bits, original_address) =
+                    apply_memory_mask(current_mask, original_address);
+
+                for iteration in 0..1 << floating_bits.len() {
+                    let mut address = original_address;
+
+                    for (i, bit_position) in floating_bits.iter().enumerate() {
+                        match (iteration & (1 << i)) >> i {
+                            0 => address &= !(1 << bit_position),
+                            1 => address |= 1 << bit_position,
+                            _ => unreachable!(),
+                        }
+
+                        memory.insert(address, value);
+                    }
+                }
+            }
+        }
+    }
+
+    Some(memory.values().sum())
 }
 
 fn main() {
@@ -57,6 +103,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = include_str!("../examples/14-2.txt");
-        assert_eq!(part_two(input), None);
+        assert_eq!(part_two(input), Some(208));
     }
 }
