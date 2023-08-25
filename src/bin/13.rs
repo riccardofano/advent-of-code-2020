cd78e87d1510259a602ddf8937e9c4cbab0c0295
Riccardo Fano <rcr.fano@gmail.com>
2023-08-25 10:24:52 +0200

    Solve first part of day 13

---
+++ src/bin/13.rs
@@ -0,0 +1,63 @@
+enum Bus {
+    X,
+    Id(usize),
+}
+
+impl Bus {
+    fn parse(input: &str) -> Self {
+        match input {
+            "x" => Bus::X,
+            n => match n.parse::<usize>() {
+                Ok(id) => Bus::Id(id),
+                Err(_) => unreachable!(),
+            },
+        }
+    }
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let mut lines = input.lines();
+    let original_timestamp: usize = lines.next().unwrap().parse().unwrap();
+    let buses: Vec<Bus> = lines.next().unwrap().split(',').map(Bus::parse).collect();
+
+    let mut current_timestamp = original_timestamp;
+    loop {
+        for bus in &buses {
+            let Bus::Id(id) = bus else {
+                continue;
+            };
+
+            if current_timestamp % id == 0 {
+                return Some((current_timestamp - original_timestamp) * id);
+            }
+        }
+        current_timestamp += 1;
+    }
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    None
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 13);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 13);
+        assert_eq!(part_one(&input), Some(295));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 13);
+        assert_eq!(part_two(&input), None);
+    }
+}
