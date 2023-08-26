f64b90003b1bbb21ce74b20ec3f950411cd83b07
Riccardo Fano <rcr.fano@gmail.com>
2023-08-26 16:53:09 +0200

    Introduce a cache for low numbers for part 2

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{hash_map::Entry, HashMap};
 
 pub fn part_one(input: &str) -> Option<usize> {
     let starting_numbers = input
@@ -30,6 +30,9 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(last_spoken)
 }
 
+const LAST_TURN_PART_TWO: usize = 30_000_000;
+const LOW_NUMBER_CACHE_BOUNDARY: usize = LAST_TURN_PART_TWO / 50;
+
 pub fn part_two(input: &str) -> Option<usize> {
     let starting_numbers = input
         .trim()
@@ -39,21 +42,35 @@ pub fn part_two(input: &str) -> Option<usize> {
 
     let mut current_turn = 1;
     let mut spoken_numbers = HashMap::<usize, usize>::new();
+    let mut low_number_cache = [0; LOW_NUMBER_CACHE_BOUNDARY];
 
     for number in starting_numbers {
-        spoken_numbers.insert(number, current_turn);
+        low_number_cache[number] = current_turn;
         current_turn += 1;
     }
 
-    // NOTE: assumes all the starting numbers are different
     let mut last_spoken = 0;
-    while current_turn != 30000000 {
-        let next_spoken = match spoken_numbers.get(&last_spoken) {
-            Some(last_turn_spoken) => current_turn - last_turn_spoken,
-            None => 0,
+    while current_turn != LAST_TURN_PART_TWO {
+        if last_spoken < LOW_NUMBER_CACHE_BOUNDARY {
+            let last_turn_spoken = &mut low_number_cache[last_spoken];
+            last_spoken = if *last_turn_spoken == 0 {
+                0
+            } else {
+                current_turn - *last_turn_spoken
+            };
+            *last_turn_spoken = current_turn;
+        } else {
+            match spoken_numbers.entry(last_spoken) {
+                Entry::Occupied(mut occupied) => {
+                    last_spoken = current_turn - occupied.insert(current_turn);
+                }
+                Entry::Vacant(vacant) => {
+                    vacant.insert(current_turn);
+                    last_spoken = 0;
+                }
+            };
         };
-        spoken_numbers.insert(last_spoken, current_turn);
-        last_spoken = next_spoken;
+
         current_turn += 1;
     }
 
@@ -79,6 +96,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 15);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(175594));
     }
 }
