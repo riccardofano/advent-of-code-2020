cda6fc7b977731d20938432fb7a7da7830a64c13
Riccardo Fano <rcr.fano@gmail.com>
2023-08-27 11:43:51 +0200

    Solve second part of day 17

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -71,8 +71,77 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(active_cubes.len())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+type Point4d = (isize, isize, isize, isize);
+
+fn parse_starting_active_cubes_two(input: &str) -> HashSet<Point4d> {
+    let mut set = HashSet::new();
+
+    input.lines().enumerate().for_each(|(y, line)| {
+        line.bytes().enumerate().for_each(|(x, byte)| {
+            if byte == b'#' {
+                set.insert((x as isize, y as isize, 0, 0));
+            }
+        })
+    });
+
+    set
+}
+
+fn step_simulation_two(active_cubes: &HashSet<Point4d>) -> HashSet<Point4d> {
+    let mut next_active_cubes = HashSet::new();
+
+    let mut might_become_active = HashMap::new();
+
+    for point in active_cubes {
+        let mut active_neighbors = 0;
+        let neighbors = get_neighbors_two(point);
+        for neighbor in neighbors {
+            if active_cubes.contains(&neighbor) {
+                active_neighbors += 1;
+            } else {
+                *might_become_active.entry(neighbor).or_insert(0) += 1;
+            }
+        }
+
+        if (2..=3).contains(&active_neighbors) {
+            next_active_cubes.insert(*point);
+        }
+    }
+
+    for (cube, active_neighbors) in might_become_active {
+        if active_neighbors == 3 {
+            next_active_cubes.insert(cube);
+        }
+    }
+
+    next_active_cubes
+}
+
+fn get_neighbors_two((x, y, z, w): &Point4d) -> HashSet<Point4d> {
+    let mut neighbors = HashSet::with_capacity(27);
+    for delta_w in -1..=1 {
+        for delta_z in -1..=1 {
+            for delta_y in -1..=1 {
+                for delta_x in -1..=1 {
+                    neighbors.insert((x - delta_x, y - delta_y, z - delta_z, w - delta_w));
+                }
+            }
+        }
+    }
+
+    neighbors.remove(&(*x, *y, *z, *w));
+    neighbors
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut active_cubes = parse_starting_active_cubes_two(input);
+
+    for _ in 0..6 {
+        dbg!(active_cubes.len());
+        active_cubes = step_simulation_two(&active_cubes);
+    }
+
+    Some(active_cubes.len())
 }
 
 fn main() {
@@ -94,6 +163,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 17);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(848));
     }
 }
